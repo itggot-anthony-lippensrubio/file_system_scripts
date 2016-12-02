597ccae34d8b852bb557f46ac15d2be5d44b700a
itggot-anthony-lippensrubio <anthony.lippensrubio@itggot.se>
2016-12-02 16:23:52 +0100

    work or not
    alla saker funkar förutom ls.rb

--- ls.rb
+++ ls.rb
@@ -1,1 +1,9 @@
-require_relative 'lib/.rb'
+# require_relative 'file_system_scripts.rar'
+
+a = ARGV[0]
+
+if a == "txt" || "rb"
+  puts Dir.glob("*.#{a}")
+else
+  puts "cyka blyat does not exist"
+end
--- touch.rb
+++ touch.rb
@@ -1,1 +1,9 @@
-require_relative 'lib/.rb'
+# require_relative 'lib/.rb
+
+a = ARGV[0]
+
+if File.exist? a
+  puts "File already exist sry m8"
+else
+  File.new(a, "w")
+end
